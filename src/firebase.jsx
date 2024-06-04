14afb65f68239f76c6020ce0be4c51a665ec476f
Nabila Maksud <nabila.maksud@gmail.com>
2024-06-04 20:09:48 +0200

    second upload after completing the requirements

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAUv-t4dNT5Kbwe0QlyDUZrcXEXOEpFihM",
-    authDomain: "blogpost-test-af519.firebaseapp.com",
-    projectId: "blogpost-test-af519",
-    storageBucket: "blogpost-test-af519.appspot.com",
-    messagingSenderId: "58804381378",
-    appId: "1:58804381378:web:88c9ee77b925cc39867d5b"
+apiKey: "AIzaSyBFuNB6ltr5DVtNyv32p2WJaBGH8QF09sc",
+  authDomain: "blogpostapp-c0c3f.firebaseapp.com",
+  projectId: "blogpostapp-c0c3f",
+  storageBucket: "blogpostapp-c0c3f.appspot.com",
+  messagingSenderId: "807111936446",
+  appId: "1:807111936446:web:19479640493bc4c3c591ef"
 };
 
 
